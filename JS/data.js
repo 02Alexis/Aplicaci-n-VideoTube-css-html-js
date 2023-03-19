dffc153f8fd0ca240c3901b4fee0a5e6b530b3de
02Alexis <m_chael1012@hotmail.com>
2023-03-19 00:00:07 -0500

    First comment

---
+++ JS/data.js
@@ -0,0 +1,110 @@
+export const youtubeVideo = [
+    {
+        id: 1,
+        name: "Explicación escena post-créditos Shazam 2 - The Top Comics".substring(0, 100),
+        thumbnail: "https://img.youtube.com/vi/6upKLF2k7zk/maxresdefault.jpg",
+        link: "https://youtu.be/6upKLF2k7zk",
+        img: "./assets/jack.png",
+        views: "224,888 de vistas &bull; Hace 1 mes", 
+        seenIn: {
+            category: "movies",
+            views: "224,888 de vistas &bull; Hace 1 mes",
+        }
+    },
+    {
+        id: 2,
+        name: "Demo de nuevo personaje de Genshin Impact - Mika: El maestro de la pluma de mapeo",
+        thumbnail: "https://img.youtube.com/vi/U8EWojkDwPc/maxresdefault.jpg",
+        link: "https://youtu.be/U8EWojkDwPc",
+        img: "./assets/jack.png",
+        views: "750,204 de vistas &bull; Hace 1 día",  
+        seenIn: {
+            category: "games",
+            views: "750,204 de vistas &bull; Hace 1 día"
+        }   
+    },
+    {
+        id: 3,
+        name: "El Origen de XBOX : La Historia en 1 Video",
+        thumbnail: "https://img.youtube.com/vi/uJzdQPBOvdY/mqdefault.jpg",
+        link: "https://youtu.be/uJzdQPBOvdY",
+        img: "./assets/jack.png",
+        views: "135,338 &bull; Hace 9 horas", 
+        seenIn: {
+            category: "games",
+            views: "135,338 de vistas &bull; Hace 9 horas"
+        }     
+    },
+    {
+        id: 4,
+        name: "Aprende Git ahora! curso completo GRATIS desde cero",
+        thumbnail: "https://img.youtube.com/vi/VdGzPZ31ts8/maxresdefault.jpg",
+        link: "https://youtu.be/VdGzPZ31ts8",
+        img: "./assets/jack.png",
+        views: "750,204 de vistas &bull; Hace 1 día",    
+        seenIn: {
+            category: "developer",
+            views: "750,204 de vistas $bull; Hace 1 día"
+        }   
+    },
+    {
+        id: 5,
+        name: "How To Make A Website With Login And Register | HTML CSS & Javascript",
+        thumbnail: "https://img.youtube.com/vi/p1GmFCGuVjw/maxresdefault.jpg",
+        link: "https://youtu.be/p1GmFCGuVjw",
+        img: "./assets/jack.png",
+        views: "330,376 de vistas &bull; Hace 1 mes",    
+        seenIn: {
+            category: "developer",
+            views: "330,376 de vistas &bull; Hace 1 mes"
+        }   
+    },
+    {
+        id: 6,
+        name: "(F2P) C0 AYAKA Freeze & C0 RAIDEN National | 3.1 Spiral Abyss Floor 12 | Genshin Impact",
+        thumbnail: "https://img.youtube.com/vi/9fPqtIZSOYk/maxresdefault.jpg",
+        link: "https://youtu.be/9fPqtIZSOYk",
+        img: "./assets/jack.png",
+        views: "96,585 de vistas &bull; Hace 5 mesese",
+        seenIn: {
+            category: "games",
+            views: "96,585 de vistas &bull; Hace 5 mesese"
+        }   
+    },
+    {
+        id: 7,
+        name: "The Last Of Us La Serie : La Historia en 1 Video",
+        thumbnail: "https://img.youtube.com/vi/APpsXYvsQt4/mqdefault.jpg",
+        link: "https://youtu.be/APpsXYvsQt4",
+        img: "./assets/jack.png",
+        views: "1 M de de vistas &bull; Hace 3 días",   
+        seenIn: {
+            category: "movies",
+            views: "1 M de vistas &bull; Hace 3 días"
+        }   
+    },
+    {
+        id: 8,
+        name: "FLASH Tráiler Español Latino (2023)",
+        thumbnail: "https://img.youtube.com/vi/8dFTgXriXWg/maxresdefault.jpg",
+        link: "https://youtu.be/8dFTgXriXWg",
+        img: "./assets/jack.png",
+        views: "1 M de vistas &bull; Hace 1 mes",    
+        seenIn: {
+            category: "movies",
+            views: "1 M de vistas &bull; Hace 1 mes"
+        }   
+    },
+    {
+        id: 9,
+        name: "Curso Django. Proyecto completo. FIN. Vídeo 70",
+        thumbnail: "https://img.youtube.com/vi/35k94ap6jqI/maxresdefault.jpg",
+        link: "https://youtu.be/35k94ap6jqI",
+        img: "./assets/jack.png",
+        views: "15,917 de vistas &bull; Hace 8 meses",
+        seenIn: {
+            category: "developer",
+            views: "15,917 de vistas &bull; Hace 8 meses"
+        }   
+    },
+];
---
+++ JS/script.js
@@ -0,0 +1,67 @@
+import { youtubeVideo } from "./data.js";
+
+console.log(youtubeVideo);
+
+const videos = JSON.parse(sessionStorage.getItem("videos")) || youtubeVideo;
+
+const containerCards = document.querySelector(".main_cards");
+console.log(containerCards);
+
+const printVideos = (container, videoList) => {
+    container.innerHTML = "";
+    videoList.forEach((video) => {
+        container.innerHTML += `
+        <article class="cards">
+            <figure class="cards__figure">
+                <img class="cards__image" 
+                data-card='cards' name=${video.id}
+                src=${video.thumbnail} alt=${video.name}>
+            </figure>
+            <div class="flex-div">
+                <figure>
+                    <img src=${video.img}>
+                </figure>
+                <h3 class="cards__name" data-card='cards' name=${video.id}>${video.name}</h3>
+            </div> 
+            <p class="cards__views" name=${video.id}>${video.views}</p>           
+        </article>
+        `;
+    });
+};
+
+//3. Vamos a escuchar al evento DomContentLoad y cuando suceda este evento se deben imprimir los personajes
+document.addEventListener("DOMContentLoaded", () => {
+    printVideos(containerCards, videos);
+});
+
+//4. Vamos a escuchar el evento click sobre las cards
+document.addEventListener("click", (event) => {
+
+    const dataCardAttribute = event.target.getAttribute("data-card");
+    if (dataCardAttribute === "cards") {
+        const id = event.target.getAttribute("name");
+        sessionStorage.setItem("idVideo", JSON.stringify(id));
+        window.location.href = "./template/details.html";
+    }
+});
+
+//-----------------------Filter---------------------
+//1. Creamos un array con las categorías de los personajes
+const categories = ["all"];
+videos.forEach((item) => {
+    if (!categories.includes(item.seenIn.category)) {
+        categories.push(item.seenIn.category);
+    }
+});
+
+categories.forEach((item) => {
+    const filterButton = document.getElementsByName(item)[0];
+  
+    filterButton.addEventListener("click", () => {
+      const filterVideos =
+        item === "all"
+          ? videos
+          : videos.filter((element) => element.seenIn.category === item);
+          printVideos(containerCards, filterVideos);
+    });
+});
